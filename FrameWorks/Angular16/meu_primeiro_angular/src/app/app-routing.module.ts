b77f7f083700773d670e2632f147fe577bcce452
AnthonyDevluk2210 <anthonydepaulacolliaso@gmail.com>
2025-05-21 11:27:09 -0300

    21.05.2025

--- FrameWorks/Angular16/meu_primeiro_angular/src/app/app-routing.module.ts
+++ FrameWorks/Angular16/meu_primeiro_angular/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { FormsComponent } from './pages/forms/forms.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   {path: "inicio", component: HomeComponent},
   { path: "produtos", component: ProductsComponent },
   {path: "contato", component: ContactComponent },
+  {path: "formulario", component: FormsComponent}
 ];
 
 @NgModule({
