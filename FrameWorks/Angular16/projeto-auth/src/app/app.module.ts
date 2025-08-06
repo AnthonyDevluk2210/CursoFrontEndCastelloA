ce213415eec09a751b94ef268047d6cce0ef05cb
AnthonyDevluk2210 <anthonydepaulacolliaso@gmail.com>
2025-08-06 11:26:58 -0300

    06.08.2025

---
+++ FrameWorks/Angular16/projeto-auth/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InternaComponent } from './pages/interna/interna.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    HomeComponent,
+    InternaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    HttpClientModule
+    ReactiveFormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
